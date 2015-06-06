974df7e6d6ac2222da89fdabadbf0ce9e842f03f
lzw981018 <lzw981018@outlook.com>
2015-06-06 14:10:37 +0800

    add a new file

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: hello.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 2015年06月06日 星期六 14时09分24秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "hello world" << endl;
+    return 0;
+}
